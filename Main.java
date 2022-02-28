8fdb84cb94f5f4c848083db1fde89ebc666bb830
perfect23 <99809191+perfect23@users.noreply.github.com>
2022-02-28 15:08:02 +0300

    Add files via upload
    Homework to 1.03.22

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+   public static void main (String[]args){
+int year = 6;
+
+
+       switch (year) {
+           case 1:
+           case 2:
+           case 12:
+               System.out.println ( "Winter");
+               break;
+           case 3:
+           case 4:
+           case 5:
+               System.out.println( "Spring");
+               break;
+           case 6:
+           case 7:
+           case 8:
+               System.out.println( "Summer");
+               break;
+           case 9:
+           case 10:
+           case 11:
+               System.out.println( "Autmn");
+               break;
+
+       }
+       // write your code here
+    }
+}
